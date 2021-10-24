7dd2ca4ed06ed71775ca4ee669fc4b7588c611bc
Sterling <sterling1@hotmail.ca>
2021-10-24 00:09:58 -0400

    Add files via upload

--- background.js
+++ background.js
@@ -1,79 +1,56 @@
-var minutes = 1;
-var seconds = 0;
-const states = {PAUSED: 'paused', STUDYING: 'studying', ONBREAK: 'onBreak', NOTWORKING: 'notWorking'};
+const states = {STUDYING: 'studying', ONBREAK: 'onBreak', NOTWORKING: 'notWorking'};
 var state = states.NOTWORKING;
 
 chrome.runtime.onMessage.addListener(function(message) {
-  if(message.extension == "opened"){
-    chrome.runtime.sendMessage(state);
-    chrome.runtime.sendMessage({time: minutes + "m " + seconds + "s"});
-  } else if(message.function == "clickedButton"){
-      clickedButton();
-  } else if (message.function == "reset"){
-      reset();
-  }
+    if(message.extension == "opened"){
+        chrome.runtime.sendMessage(state);
+        //Too lazy to add time display rn
+        //chrome.runtime.sendMessage({time: minutes + "m " + seconds + "s"});
+    } else if(message.function == "clickedButton"){
+        clickedButton();
+    } else if (message.function == "reset"){
+        reset();
+    }
+});
 
+chrome.alarms.onAlarm.addListener(function(alarm) {
+    console.log("Got an alarm!", alarm);
+    if(alarm.name == "Studying"){
+        chrome.alarms.clearAll();
+        state = states.ONBREAK;
+        chrome.notifications.create(
+            "TakeABreak",
+            {
+                type: 'basic',
+                iconUrl: 'random128.png',
+                title: "Take A Break!",
+                message: "Great job studying!"
+            }
+        );
+        chrome.alarms.create("OnBreak", {delayInMinutes: 1});
+    }else if(alarm.name == "OnBreak"){
+        chrome.alarms.clearAll();
+        state = states.STUDYING;
+        chrome.notifications.create(
+            "BackToStudying",
+            {
+                type: 'basic',
+                iconUrl: 'random128.png',
+                title: "Back To Studying!!",
+                message: "Let's get back to work!"
+            }
+        );
+        chrome.alarms.create("Studying", {delayInMinutes: 2});
+    }
 });
 
 function clickedButton() {
-  countDown();
-  if(state == states.NOTWORKING || state == states.ONBREAK || state == states.PAUSED){
-      state = states.STUDYING;
-  }else if(state = states.STUDYING){
-      states = states.PAUSED;
-  }
+    chrome.alarms.create("Studying", {delayInMinutes: 2});
+    state = states.STUDYING;
 }
 
-function countDown(){
-  var timer = setInterval(function(){
-      if(state == states.STUDYING){
-          if(seconds == 0 ){
-              seconds = 59;
-              if(minutes == 0){
-                  BreakTime();
-              }else{
-                  minutes -= 1;
-              }
-          }else{
-              seconds -= 1;
-          }
-      } else if(state == states.ONBREAK){
-          if(seconds == 59){
-              seconds = 0;
-              minutes +=1;
-              if(minutes == 4){
-                  BreakOverAlert();
-              }else if(minutes == 5){
-                  LongBreakAlert();
-              }
-          }else{
-              seconds += 1;
-          }
-      } else if(state == states.PAUSED || state == states.NOTWORKING){
-          clearInterval(timer);
-      }
-      let timerText = minutes + "m " + seconds + "s";
-      chrome.runtime.sendMessage({time: timerText});
-  },1000);
-}
 
 function reset(){
-    minutes = 25;
-    seconds = 0;
-}
-
-function BreakTime(){
-    state = states.ONBREAK;
-    minutes = 5;
-    seconds = 0;
-}
-
-function breakNotification(){
-    chrome.notifications.create(
-        "TakeABreak",
-        {
-            title: "Take A Break!",
-            message: "Great job studying!"
-        }
-    );
+    chrome.alarms.clearAll();
+    state = states.NOTWORKING;
 }
--- popup.js
+++ popup.js
@@ -1,64 +1,52 @@
 var mainButton = document.getElementById("MainButton");
 var resetButton = document.getElementById("reset");
 var time = document.getElementById("time");
-var minutes = 2;
-var seconds = 0;
-var onBreak = false;
-var paused = false; //YOu know for like keeping track of which state it is in.
 var currentState = document.getElementById("CurrentState");
+var endTime;
 //var port = chrome.runtime.connect({name: "timer"});
 
-const states = {PAUSED: 'paused', STUDYING: 'studying', ONBREAK: 'onBreak', NOTWORKING: 'notWorking'}
+const states = {STUDYING: 'studying', ONBREAK: 'onBreak', NOTWORKING: 'notWorking'}
 var state = states.NOTWORKING;
 
 chrome.runtime.sendMessage({extension: "opened"});
     chrome.runtime.onMessage.addListener(function(message) {
-        //Gets state of crap
-        if(message.state == states.STUDYING){
+        //Fixes: Fixed comments from last time not being helpful
+        
+        if(message == states.STUDYING){
             state = states.STUDYING;
-            mainButton.innerHTML = "Pause timer";
+            mainButton.innerHTML = "No Touchy Timer";
             currentState.innerHTML = "Good luck studying!"
             currentState.style.color = "black";
             time.style.color = "black";
-        }else if(message.state == states.PAUSED){
-            state = state.PAUSED;
-            mainButton.innerHTML = "Continue Timer";
-            currentState.innerHTML = "Paused.";
-        }else if(message.state == states.ONBREAK){
+            mainButton.disabled = true;
+        }else if(message == states.ONBREAK){
             state = state.ONBREAK;
             mainButton.innerHTML = "Break time!";
-            currentState.innerHTML = "Continue Timer";
-        }else if(message.state == states.NOTWORKING){
+            currentState.innerHTML = "No Touchy Timer";
+            mainButton.disabled = true;
+        }else if(message == states.NOTWORKING){
             state = states.NOTWORKING;
             mainButton.innerHTML = "Not working!";
             currentState.innerHTML = "Start Timer";
-        }else if(message.time != null){
-            time.innerHTML = message.time;
-        }else if(message.function == "breakOver"){
-            BreakOverAlert();
-        }else if(message.function == "longBreak"){
-            LongBreakAlert();
-        }
+        }/*else if(message.time != null){
+            endTime = message.time;
+        }*/
     });
     
 mainButton.onclick = function() {
     chrome.runtime.sendMessage({function: "clickedButton"});
+    state = states.STUDYING;
+    mainButton.innerHTML = "No Touchy Timer";
+    currentState.innerHTML = "Good luck studying!"
+    currentState.style.color = "black";
+    time.style.color = "black";
+    mainButton.disabled = true;
 }
 
 resetButton.onclick = function() {
     state = states.NOTWORKING;
     currentState.innerHTML = "Not studying";
     currentState.style.color = "black";
+    mainButton.disabled = false;
     chrome.runtime.sendMessage({function: "reset"});
-}
-
-
-function BreakOverAlert(){
-    currentState.innerHTML = "Your break is over"
-
-}
-function LongBreakAlert(){
-    currentState.innerHTML = "Your break is going a little long."
-    currentState.style.color = "red";
-    time.style.color = "red";
 }
